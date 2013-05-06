514131434dea469dc87813fea309df4483f07f43
Chris Caruso <carusocr@ldc.upenn.edu>
2013-05-06 15:29:05 -0400

    added commentary on old perl script, renamed getstream

---
+++ streamcap.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+=begin
+
+How I understand the Perl script to work:
+
+1. Source hash initialized.
+2. Output hash initialized.
+3. Specific source passed via command line argument
+4. Output file variable set up via constants and output hash.
+5. clvc argument array joined in with cvlc inside eval statement:
+		   my $ccc = sprintf("%s -l %s -c %s%s%s 2>&1 >%s",
+          SU, SU_NAME,'"', join(' ',CVLC,@cvlc_args), '"', "/dev/null" );
+6. Once clvc command is set:
+			my $vlcpid = fork();
+			if ($vldpic==0){ exec($ccc)};
+=end
+
+require 'nokogiri'
+
+sources = Hash.new
+doc = Nokogiri::XML(File.open("getstream.xml"))
+doc.xpath('//SrcDef').each do |node|
+	sources[:src] = node.xpath('Src').text
+	sources[:url] = node.xpath('Url').text
+#	puts sources[:src], sources[:url]
+end
