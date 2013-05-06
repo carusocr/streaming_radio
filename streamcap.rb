0a45c4d370e5fccd8fa707bd35d4db706d0615b7
Chris Caruso <carusocr@ldc.upenn.edu>
2013-05-06 15:49:24 -0400

    .

--- streamcap.rb
+++ streamcap.rb
@@ -20,7 +20,7 @@ require 'nokogiri'
 
 sources = Hash.new
 doc = Nokogiri::XML(File.open("getstream.xml"))
-doc.xpath('//SrcDef').each do |node|
+doc.xpath('//SrcDef').children.each do |node|
 	sources[:src] = node.xpath('Src').text
 	sources[:url] = node.xpath('Url').text
 #	puts sources[:src], sources[:url]
