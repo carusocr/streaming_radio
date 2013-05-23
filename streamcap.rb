287cc1a7db8bfbb5ad02d96f5d2886a6722570cd
carusocr <carusocr@ldc.upenn.edu>
2013-05-23 13:32:42 -0400

    added killproc method

--- streamcap.rb
+++ streamcap.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+#Streaming radio capture script
+#Author: Chris Caruso
+# May 2013
 
 =begin
 
@@ -15,7 +18,6 @@ abort "You must enter an iso639 language code!" unless ARGV[0]
 src_dir = "/lre14/bin/streaming"
 src_dir = "."
 config_file = "getstream_#{ARGV[0]}.xml"
-su_name = 'walkerk'
 MPLAYER = '/usr/bin/mplayer'
 RECDIR = '/mnt/drobo/12/streaming';
 REC_DURATION = 1700;
@@ -32,23 +34,33 @@ def download_stream(src_name,src_url,src_lang)
 	puts src_url, src_name, src_lang
 end
 
+def killprocs(src_name) # <--- change this to src_url after testing! ***
+	targets = (`ps -fC vim | grep '#{src_name}' | awk '{print $2}'`).split
+	targets.each do |t|
+		puts "Killing process #{t}...\n"	
+	end
+end
+
 # Loop over set of sources, check to make sure that 
 # no other download processes for that source are running,
 # kick off new process.
 
 sources.keys.each do |s|
+
 	src_name = sources[s][0]
 	src_url = sources[s][1]
 	src_lang = sources[s][2]
+	killprocs(src_name) # Kill any existing downloads.
 	timestring = Time.now.strftime("%Y%m%d_%H%M%S")
 	cmd = "#{MPLAYER} #{src_url} -cache 8192 -dumpstream -dumpfile #{timestring}_#{src_name}_#{src_lang}.mp3\n"
-	#testing out process ID capture and forking.
-	src_pid = Process.fork {}
+	#fork each source download and record PID in hash
+	src_pid = Process.fork {download_stream(src_name,src_url,src_lang)}
 	sources[s][3] = src_pid
-	#puts "Source PID is #{src_pid} for #{src_name} download process.\n"
-	download_stream(src_name,src_url,src_lang)
+	puts "Source PID is #{src_pid} for #{src_name} download process.\n"
+	#download_stream(src_name,src_url,src_lang)
+
 end
-#sources.keys.each do |s|
-#	puts sources[s][3]
-#end
+sources.keys.each do |s|
+	puts sources[s][3]
+end
 
