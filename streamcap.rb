2960148f0b8be27c2bd192772095ae47a03c1164
carusocr <carusocr@ldc.upenn.edu>
2013-05-28 16:35:45 -0400

    removed src_lang from hash, not needed

--- streamcap.rb
+++ streamcap.rb
@@ -24,16 +24,17 @@ RECDIR = '/mnt/drobo/12/streaming';
 REC_DURATION = 1700;
 sources = Hash.new
 doc = Nokogiri::XML(File.open("#{src_dir}/#{config_file}"))
-#parse xml, check langcode first and pop hash only with argv'ed lang
+
+#parse xml, check langcode first and populate hash only with argv'ed lang
 doc.xpath('//SrcDef/ISOLangCode').each do |node|
 	if node.text =~ /#{lang}/
 		srcinfo = node.xpath('parent::node()').text.split("\n")
 		srcinfo.map{|x| x.strip!}
-		sources["#{node.xpath('../@id')}"] = (srcinfo.reject{|x| x.length==0})
+		sources["#{node.xpath('../@id')}"] = (srcinfo.reject{|x| x.length==0 || x == lang})
 	end
 end
 
-def download_stream(cmd, src_name,src_url,src_lang)
+def download_stream(cmd)
 #add actual download here
 	puts "Download command is #{cmd}\n"
 end
@@ -43,7 +44,7 @@ def killprocs(src_name) # <--- change this to src_url after testing! ***
 	targets = (`ps -fC vim | grep '#{src_name}' | awk '{print $2}'`).split
 	targets.each do |t|
 		# kill procnum
-		puts "Killing process #{t}...\n"	
+		puts "Killing \##{t}, existing #{src_name} process...\n"	
 	end
 
 end
@@ -56,12 +57,11 @@ sources.keys.each do |s|
 
 	src_name = sources[s][0]
 	src_url = sources[s][1]
-	src_lang = sources[s][2]
-	#killprocs(src_name) # Kill any existing downloads.
+	killprocs(src_name) # Kill any existing downloads.
 	timestring = Time.now.strftime("%Y%m%d_%H%M%S")
-	cmd = "#{MPLAYER} #{src_url} -cache 8192 -dumpstream -dumpfile #{timestring}_#{src_name}_#{src_lang}.mp3\n"
+	cmd = "#{MPLAYER} #{src_url} -cache 8192 -dumpstream -dumpfile #{timestring}_#{src_name}_#{lang}.mp3\n"
 	#fork each source download and record PID in hash
-	src_pid = Process.fork {download_stream(cmd, src_name,src_url,src_lang)}
+	src_pid = Process.fork {download_stream(cmd)}
 	sources[s][3] = src_pid
 
 end
