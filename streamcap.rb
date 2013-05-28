ab5e6fb334db3be624e987971f3af1b17ccb22da
Chris Caruso <carusocr@ldc.upenn.edu>
2013-05-28 16:16:32 -0400

    changed xml parse so all langs can be in single config file

--- streamcap.rb
+++ streamcap.rb
@@ -17,28 +17,35 @@ require 'time'
 abort "You must enter an iso639 language code!" unless ARGV[0]
 src_dir = "/lre14/bin/streaming"
 src_dir = "."
-config_file = "getstream_#{ARGV[0]}.xml"
+lang = ARGV[0]
+config_file = "getstream.xml"
 MPLAYER = '/usr/bin/mplayer'
 RECDIR = '/mnt/drobo/12/streaming';
 REC_DURATION = 1700;
 sources = Hash.new
 doc = Nokogiri::XML(File.open("#{src_dir}/#{config_file}"))
-doc.xpath('//SrcDef').each do |node|
-	node.xpath('./@id')
-	srcinfo = node.xpath('child::node()').text.split("\n")
-	srcinfo.map{|x| x.strip!}
-	sources["#{node.xpath('./@id')}"] = (srcinfo.reject{|x| x.length==0})
+#parse xml, check langcode first and pop hash only with argv'ed lang
+doc.xpath('//SrcDef/ISOLangCode').each do |node|
+	if node.text =~ /#{lang}/
+		srcinfo = node.xpath('parent::node()').text.split("\n")
+		srcinfo.map{|x| x.strip!}
+		sources["#{node.xpath('../@id')}"] = (srcinfo.reject{|x| x.length==0})
+	end
 end
 
-def download_stream(src_name,src_url,src_lang)
-	puts src_url, src_name, src_lang
+def download_stream(cmd, src_name,src_url,src_lang)
+#add actual download here
+	puts "Download command is #{cmd}\n"
 end
 
 def killprocs(src_name) # <--- change this to src_url after testing! ***
+
 	targets = (`ps -fC vim | grep '#{src_name}' | awk '{print $2}'`).split
 	targets.each do |t|
+		# kill procnum
 		puts "Killing process #{t}...\n"	
 	end
+
 end
 
 # Loop over set of sources, check to make sure that 
@@ -50,17 +57,11 @@ sources.keys.each do |s|
 	src_name = sources[s][0]
 	src_url = sources[s][1]
 	src_lang = sources[s][2]
-	killprocs(src_name) # Kill any existing downloads.
+	#killprocs(src_name) # Kill any existing downloads.
 	timestring = Time.now.strftime("%Y%m%d_%H%M%S")
 	cmd = "#{MPLAYER} #{src_url} -cache 8192 -dumpstream -dumpfile #{timestring}_#{src_name}_#{src_lang}.mp3\n"
 	#fork each source download and record PID in hash
-	src_pid = Process.fork {download_stream(src_name,src_url,src_lang)}
+	src_pid = Process.fork {download_stream(cmd, src_name,src_url,src_lang)}
 	sources[s][3] = src_pid
-	puts "Source PID is #{src_pid} for #{src_name} download process.\n"
-	#download_stream(src_name,src_url,src_lang)
 
 end
-sources.keys.each do |s|
-	puts sources[s][3]
-end
-
