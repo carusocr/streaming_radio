df73bf339713ad50fbfbf06266c098eb10e1d496
Chris Caruso <carusocr@ldc.upenn.edu>
2013-05-31 09:45:32 -0400

    postproc

--- process_bc.sh
+++ process_bc.sh
@@ -2,5 +2,5 @@ day=`date -d 'yesterday' +%Y%m%d`
 procdir="/lre14-collection/audio/incoming"
 for i in `ls /v20/bn_collection/$day*A.wav | sed s/.wav//`; do
   echo -e "Processing $i...\n"
-  `flac $i.wav -o $procdir/$i.flac`  
+  flac $i.wav -o $procdir/$i.flac
 done
